86ad8d1cb857a64e3bc62d16388f427b0bfa05e1
jaypume <i@jaypu.com>
2015-11-05 10:31:11 +0800

    增加了monokai主题

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -4,6 +4,8 @@
 (add-to-list
  'load-path
  (expand-file-name "lisp" user-emacs-directory))
+;; 加载theme文件夹
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
 ;; 下面每一个被require的feature都对应一个lisp/目录下的同名
 ;; elisp文件，例如init-utils.el、init-elpa.el
 ;;(require 'init-utils)   ;; 为加载初始化文件提供一些自定义的函数和宏
@@ -18,3 +20,17 @@
 ;;(require 'init-markdown)
 ;;(require 'init-auctex)
 ;;(provide 'init)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("094f2c4dc01b7ebe70075ab7dba2e3f0fbab788af38ec574b2939c9454fed996" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
